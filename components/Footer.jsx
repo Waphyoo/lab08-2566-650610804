9e156fefc57252451769c6c0e178287e140c9525
Waphyoo <nonny2246@gmail.com>
2023-07-17 19:40:31 +0700

    okok

---
+++ components/Footer.jsx
@@ -0,0 +1,9 @@
+export const Task = (input) => {
+  return (
+    <div class="mt-3">
+      <p className="text-secondary text-center">
+        copyright © {input.year} {input.fullName} {input.studentId}
+      </p>
+    </div>
+  );
+};
